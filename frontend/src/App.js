9ece18a14f953829bde264c53f466e8aa8ea1b3d
da_chava <diegocb1797@gmail.com>
2022-04-11 01:30:12 -0600

    Basic test data, basic product list.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,33 @@
+import data from './data';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
         <a href="/">AWebCommerce Yeah!</a>
       </header>
-      <main>List Products</main>
+      <main>
+        <h1>¡Los más vendidos!</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name}></img>
+              </a>
+
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Al Carrito</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,56 @@
+const data = {
+  products: [
+    {
+      name: 'Scars on Broadway',
+      slug: 'scars-on-broadway',
+      category: 'CD',
+      image: '/images/p1.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Amoeba Music',
+      rating: '4.5',
+      numReviews: 10,
+      description:
+        '¡Daron Malakian y su nuevo projecto solista Scars on Broadway esta pateando al mundo entero!',
+    },
+    {
+      name: 'Static-X - Start a War',
+      slug: 'staticx-start-awar',
+      category: 'CD',
+      image: '/images/p2.jpg',
+      price: 100,
+      countInStock: 8,
+      brand: 'Amoeba Music',
+      rating: '4.4',
+      numReviews: 8,
+      description: 'Un clasico del Metal Industrial y la nueva ola de Nu Metal',
+    },
+    {
+      name: 'Pain - Coming Home',
+      slug: 'pain-cominghome',
+      category: 'Vinyl',
+      image: '/images/p3.jpg',
+      price: 150,
+      countInStock: 5,
+      brand: 'Nuclear Blast',
+      rating: '4.8',
+      numReviews: 10,
+      description:
+        '¡Peter Tagtgren vuelve con su proyecto solista Pain... y con melodías mas fuertes que nunca!',
+    },
+    {
+      name: 'Hypocrisy - Worship',
+      slug: 'hypocrisy-worship-box',
+      category: 'Boxset',
+      image: '/images/p4.jpg',
+      price: 300,
+      countInStock: 4,
+      brand: 'Nuclear Blast',
+      rating: '4.8',
+      numReviews: 10,
+      description:
+        'Los maestros del death sueco vuelven con una invasión de metal que te transportará a otra galaxia.',
+    },
+  ],
+};
+export default data;
