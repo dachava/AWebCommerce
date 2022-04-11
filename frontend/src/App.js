42fd4006e6769d4ba5feaf898091c31d601a2c8b
da_chava <diegocb1797@gmail.com>
2022-04-11 00:38:31 -0600

    Baby React app.

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">AWebCommerce Yeah!</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
