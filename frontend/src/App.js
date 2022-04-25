c0eab83ee7479a825600e0e5d9f5e00df2629445
andresator <achavarria70005@ufide.ac.cr>
2022-04-24 23:06:30 -0600

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import { LinkContainer } from 'react-router-bootstrap';
 import { useContext } from 'react';
 import { Store } from './Store';
 import CartScreen from './screens/CartScreen';
+import SigningScreen from './screens/SigningScreen';
 
 function App() {
   const { state } = useContext(Store);
